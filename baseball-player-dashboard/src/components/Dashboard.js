d906e05c3a646f2254ce148dfe150fcea106c359
JCRN <clemons.jason@gmail.com>
2019-08-08 13:49:14 -0700

    added semantic-ui-react/css

---
+++ baseball-player-dashboard/src/components/Dashboard.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+
+export default function Dashboard() {}
+const [balls, setBalls] = useState(0)
+const [fouls, setFouls] = useState(0)
+const [strikes, setStrikes] = useState(0)
+
+return <Table compact celled />
